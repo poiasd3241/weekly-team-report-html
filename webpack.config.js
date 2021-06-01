8cedeb57993f2d9992a5fae4d70df0e952888052
Arseny Ivasyuk <poiasd3241@gmail.com>
2021-06-01 21:12:00 +0300

    Finish launch-guide page
    fix aside, add top nav

--- webpack.config.js
+++ webpack.config.js
@@ -52,9 +52,14 @@ const config = {
         test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
         type: "asset",
       },
-
       // Add your rules for custom modules here
       // Learn more about loaders from https://webpack.js.org/loaders/
+      {
+        test: /\.html$/,
+        use:[{
+          loader: 'html-loader?interpolate'
+        }]
+      },
     ],
   },
 };
